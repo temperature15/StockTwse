6d4881ed63655690bcedba06c6b7fd5373cc2be0
Jerry Chen <pths305@yahoo.com.tw>
2024-01-31 16:19:50 +0800

    建立 index

---
+++ StockTwse/StockTwse/Controllers/StockTwseController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StockTwse.Controllers {
+	public class StockTwseController : Controller {
+		public IActionResult Index() {
+			return View();
+		}
+	}
+}
--- StockTwse/StockTwse/Program.cs
+++ StockTwse/StockTwse/Program.cs
@@ -37,7 +37,7 @@ namespace StockTwse {
 
 			app.MapControllerRoute(
 				name: "default",
-				pattern: "{controller=Home}/{action=Index}/{id?}");
+				pattern: "{controller=StockTwse}/{action=Index}/{id?}");
 			app.MapRazorPages();
 
 			app.Run();
